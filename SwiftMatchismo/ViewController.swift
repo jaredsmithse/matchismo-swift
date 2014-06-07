ead1980ba310c55adbad51b009c4d30b153bb33d
Jared Smith <jared@zenpayroll.com>
2014-06-07 12:04:15 -0700

    Initial Commit

---
+++ SwiftMatchismo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMatchismo
+//
+//  Created by Jared Smith on 6/7/14.
+//  Copyright (c) 2014 jaredsmithse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
